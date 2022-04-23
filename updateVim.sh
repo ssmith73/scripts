117225aaddd26c5c51a3e124d54d0dbebaa0eb3d
Sean Smith <ssmith73@gmail.com>
2022-04-23 10:13:32 +0100

    Updated scripts to fix bash setup

--- updateVim.sh
+++ updateVim.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 cd ~
+rm -rf scripts*
+git clone https://github.com/ssmith73/scripts.git
 rm -rf vim*
 git clone https://github.com/ssmith73/vim2015.git
-mv ./vim2015/.vimrc .
+cp ./vim2015/.vimrc .
 rm -rf .vim/*
 echo "Installing plugins..."
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vi -c 'PluginInstall' -c ':qa'
 clear
 clear
-echo "Done!"
+echo "Setting up Bash!"
+sleep 2
+rm -rf .bash
+rm -f .bashrc
+cp -r ./scripts/.bash .
+cp ./scripts/.bashrc .
+clear
+clear
+echo "Done!!"
+
+
